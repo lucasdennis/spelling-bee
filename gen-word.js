aa387e75fa56ad368750527df328d5f5c9168cf1
Lucas Dennis <lucasdennis@gmail.com>
2021-11-30 18:51:03 -0600

    Update gen-word.js

--- gen-word.js
+++ gen-word.js
@@ -9,3 +9,25 @@ function getEasyWord(){
 var randomItem = easyWords[Math.floor(Math.random()*easyWords.length)];
   document.getElementById("word").innerText = randomItem;
 }
+
+function getMediumWord(){
+  var mediumWords = [
+  "Art",
+  "Schol",
+  "Medium"
+];
+
+var randomItem = mediumWords[Math.floor(Math.random()*mediumWords.length)];
+  document.getElementById("word").innerText = randomItem;
+}
+
+function getHardWord(){
+  var hardWords = [
+  "Hard",
+  "School",
+  "Gymnastics"
+];
+
+var randomItem = hardWords[Math.floor(Math.random()*hardWords.length)];
+  document.getElementById("word").innerText = randomItem;
+}
